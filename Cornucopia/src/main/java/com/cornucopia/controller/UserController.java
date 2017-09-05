b88b5b0cba3442181e75afff54683cf94b5aa232
cuizhuangzhuang <13986374536@163.com>
2017-09-05 17:33:29 +0800

    9.5

--- Cornucopia/src/main/java/com/cornucopia/controller/UserController.java
+++ Cornucopia/src/main/java/com/cornucopia/controller/UserController.java
@@ -1,5 +1,66 @@
 package com.cornucopia.controller;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("item")
+public class UserController {
+	@RequestMapping("Index")
+	public String Index(){
+		return "Index";
+	}
+	@RequestMapping("about")
+	public String about(){
+		return "About";
+	}
+	@RequestMapping("Contact")
+	public String Contact(){
+		return "Contact";
+	}
+	@RequestMapping("Example")
+	public String Example(){
+		return "Example";
+	}
+	@RequestMapping("Join")
+	public String Join(){
+		return "Join";
+	}
+	@RequestMapping("NewsC")
+	public String NewsC(){
+		return "News-Content";
+	}
+	@RequestMapping("News")
+	public String News(){
+		return "News";
+	}
+	
+	@RequestMapping("Login")
+	public String Login(){
+		return "Login";
+	}
+	
+	@RequestMapping("Product1")
+	public String Product1(){
+		return "Product1";
+	}
+	
+	@RequestMapping("Product2")
+	public String Product2(){
+		return "Product2";
+	}
+	@RequestMapping("Product3")
+	public String Product3(){
+		return "Product3";
+	}
+	@RequestMapping("Solution")
+	public String Solution(){
+		return "Solution";
+	}
+	
+	@RequestMapping("About")
+	public String About(){
+		return "About";
+	}
+	
 }
